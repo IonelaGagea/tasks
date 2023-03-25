509253484d25979f669bc1066b4b3d56519cebf5
IonelaGagea <i.gagea@yahoo.com>
2023-03-25 21:51:08 +0200

    Tests

--- src/main/java/tasks/services/TasksService.java
+++ src/main/java/tasks/services/TasksService.java
@@ -19,6 +19,7 @@ public class TasksService {
     public ObservableList<Task> getObservableList(){
         return FXCollections.observableArrayList(tasks.getAll());
     }
+
     public String getIntervalInHours(Task task){
         int seconds = task.getRepeatInterval();
         int minutes = seconds / DateService.SECONDS_IN_MINUTE;
@@ -26,6 +27,7 @@ public class TasksService {
         minutes = minutes % DateService.MINUTES_IN_HOUR;
         return formTimeUnit(hours) + ":" + formTimeUnit(minutes);//hh:MM
     }
+
     public String formTimeUnit(int timeUnit){
         StringBuilder sb = new StringBuilder();
         if (timeUnit < 10) sb.append("0");
---
+++ src/test/java/tasks/services/TasksServiceTest.java
@@ -0,0 +1,89 @@
+package tasks.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import tasks.model.ArrayTaskList;
+import tasks.model.Task;
+
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TasksServiceTest {
+
+    TasksService service;
+
+    @BeforeEach
+    void setUp() {
+        service = new TasksService(new ArrayTaskList());
+
+    }
+
+    @Test
+    void parseFromStringToSeconds_ECP_valid() {
+        String seconds = "4:23";
+
+        int result = service.parseFromStringToSeconds(seconds);
+
+        assertEquals(15780, result);
+    }
+
+    @Test
+    void parseFromStringToSeconds_ECP_invalid() {
+        String seconds = "j4:23";
+
+        try {
+            service.parseFromStringToSeconds(seconds);
+        } catch (NumberFormatException ex) {
+            assert (true);
+        }
+    }
+
+    @Test
+    void parseFromStringToSeconds_BVA_valid_lowerBound() {
+        String seconds = "00:00";
+
+        int result = service.parseFromStringToSeconds(seconds);
+
+        assertEquals(0, result);
+    }
+
+    @Test
+    void parseFromStringToSeconds_BVA_valid_upperBound() {
+        String seconds = "23:59";
+
+        int result = service.parseFromStringToSeconds(seconds);
+
+        assertEquals(86340, result);
+    }
+
+    @Test
+    void parseFromStringToSeconds_BVA_invalid_wrongInput() {
+        String seconds = "12.00";
+
+        try {
+            service.parseFromStringToSeconds(seconds);
+        } catch (NumberFormatException ex) {
+            assert (true);
+        }
+    }
+
+    @Test
+    @Disabled
+    void parseFromStringToSeconds_BVA_invalid_outOfRange() {
+        String seconds = "24:00";
+
+        try {
+            service.parseFromStringToSeconds(seconds);
+        } catch (IllegalArgumentException ex) {
+            assert (true);
+        }
+
+    }
+
+
+}
