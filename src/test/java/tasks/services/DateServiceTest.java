1e8697556a9a450a05fee3ba4892d21e271d48f6
IonelaGagea <i.gagea@yahoo.com>
2023-03-28 15:29:06 +0300

    annotations

--- src/test/java/tasks/services/DateServiceTest.java
+++ src/test/java/tasks/services/DateServiceTest.java
@@ -1,23 +1,23 @@
 package tasks.services;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import tasks.model.ArrayTaskList;
 
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class DateServiceTest {
 
     DateService dateService;
-    private String formatErrMsg = "Invalid time format.";
-    private String boundsErrMsg = "time unit exceeds bounds";
-    private String valueErrMsg = "Invalid values for hh or mm.";
-    private String dateErrMsg = "Invalid Date.";
+    private static final String formatErrMsg = "Invalid time format.";
+    private static final String boundsErrMsg = "time unit exceeds bounds";
+    private static final String valueErrMsg = "Invalid values for hh or mm.";
+    private static final String dateErrMsg = "Invalid Date.";
 
 
     @BeforeEach
@@ -31,51 +31,48 @@ class DateServiceTest {
     void tearDown() {
     }
 
-    @Test
-    void getDateMergedWithTime_validData_OK_ECP_1() {
-        try {
-            // setup
-            String time = "20:30";
-            String pattern = "dd/MM/yyyy";
-            String timePattern = "HH:mm";
-            SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
-            SimpleDateFormat resultFormat = new SimpleDateFormat(pattern + " " + timePattern);
-            Date date = dateFormat.parse("21/03/2023");
+    @ParameterizedTest
+    @ValueSource(strings = { "20:30", "21:30" })
+    @Tag("valid")
+    void getDateMergedWithTime_validData_OK_ECP_1(String time) throws Exception {
+        // setup
+        String pattern = "dd/MM/yyyy";
+        String timePattern = "HH:mm";
+        SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
+        SimpleDateFormat resultFormat = new SimpleDateFormat(pattern + " " + timePattern);
+        Date date = dateFormat.parse("21/03/2023");
 
-            // act
-            Date result = dateService.getDateMergedWithTime(time, date);
+        // act
+        Date result = dateService.getDateMergedWithTime(time, date);
 
-            // assert
-            String formattedResult = resultFormat.format(result);
-            assert (formattedResult.equals("21/03/2023 20:30"));
-        } catch (Exception ex) {
-            assert (false);
-        }
+        // assert
+        String formattedResult = resultFormat.format(result);
+        assertEquals("21/03/2023 " + time, formattedResult);
+
     }
 
+    @Tag("valid")
     @Test
-    void getDateMergedWithTime_validData_OK_ECP_2() {
-        try {
-            // setup
-            String time = "10:59";
-            String pattern = "dd/MM/yyyy";
-            String timePattern = "HH:mm";
-            SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
-            SimpleDateFormat resultFormat = new SimpleDateFormat(pattern + " " + timePattern);
-            Date date = dateFormat.parse("21/03/2023");
+    void getDateMergedWithTime_validData_OK_ECP_2() throws Exception {
+        // setup
+        String time = "10:59";
+        String pattern = "dd/MM/yyyy";
+        String timePattern = "HH:mm";
+        SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
+        SimpleDateFormat resultFormat = new SimpleDateFormat(pattern + " " + timePattern);
+        Date date = dateFormat.parse("21/03/2023");
 
-            // act
-            Date result = dateService.getDateMergedWithTime(time, date);
+        // act
+        Date result = dateService.getDateMergedWithTime(time, date);
 
-            // assert
-            String formattedResult = resultFormat.format(result);
-            assert (formattedResult.equals("21/03/2023 10:59"));
+        // assert
+        String formattedResult = resultFormat.format(result);
+        assertEquals("21/03/2023 10:59", formattedResult);
 
-        } catch (Exception ex) {
-            assert (false);
-        }
+
     }
 
+    @Tag("invalid")
     @Test
     void getDateMergedWithTime_InvalidTime_Exception_ECP_1() {
         Exception exception = assertThrows(Exception.class, () -> {
@@ -86,7 +83,9 @@ class DateServiceTest {
         assertEquals(formatErrMsg, errorMessage);
     }
 
-    @Test
+    @RepeatedTest(value = 3, name = "{displayName} {currentRepetition}/{totalRepetitions}")
+    @DisplayName("RepeatingTest")
+    @Tag("invalid")
     void getDateMergedWithTime_InvalidTime_Exception_ECP_2() {
         Exception exception = assertThrows(Exception.class, () -> {
             dateService.getDateMergedWithTime("aa:59", new Date());
@@ -96,54 +95,49 @@ class DateServiceTest {
         assertEquals(valueErrMsg, errorMessage);
     }
 
+    @Tag("valid")
     @Test
-    void getDateMergedWithTime_validData_OK_BVA_1() {
-        try {
-            // setup
-            String time = "23:59";
-            String pattern = "dd/MM/yyyy";
-            String timePattern = "HH:mm";
-            SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
-            SimpleDateFormat resultFormat = new SimpleDateFormat(pattern + " " + timePattern);
-            Date date = dateFormat.parse("21/03/2023");
+    void getDateMergedWithTime_validData_OK_BVA_1() throws Exception {
+        // setup
+        String time = "23:59";
+        String pattern = "dd/MM/yyyy";
+        String timePattern = "HH:mm";
+        SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
+        SimpleDateFormat resultFormat = new SimpleDateFormat(pattern + " " + timePattern);
+        Date date = dateFormat.parse("21/03/2023");
 
-            // act
-            Date result = dateService.getDateMergedWithTime(time, date);
+        // act
+        Date result = dateService.getDateMergedWithTime(time, date);
 
-            // assert
-            String formattedResult = resultFormat.format(result);
-            assert (formattedResult.equals("21/03/2023 23:59"));
+        // assert
+        String formattedResult = resultFormat.format(result);
+        assertEquals("21/03/2023 23:59", formattedResult);
 
-        } catch (Exception ex) {
-            assert (false);
-        }
     }
 
+    @Tag("valid")
     @Test
-    void getDateMergedWithTime_validData_OK_BVA_2() {
-        try {
-            // setup
-            String time = "00:00";
-            String pattern = "dd/MM/yyyy";
-            String timePattern = "HH:mm";
-            SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
-            SimpleDateFormat resultFormat = new SimpleDateFormat(pattern + " " + timePattern);
-            Date date = dateFormat.parse("21/03/2023");
+    void getDateMergedWithTime_validData_OK_BVA_2() throws Exception {
+        // setup
+        String time = "00:00";
+        String pattern = "dd/MM/yyyy";
+        String timePattern = "HH:mm";
+        SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
+        SimpleDateFormat resultFormat = new SimpleDateFormat(pattern + " " + timePattern);
+        Date date = dateFormat.parse("21/03/2023");
 
-            // act
-            Date result = dateService.getDateMergedWithTime(time, date);
+        // act
+        Date result = dateService.getDateMergedWithTime(time, date);
 
-            // assert
-            String formattedResult = resultFormat.format(result);
-            assert (formattedResult.equals("21/03/2023 00:00"));
+        // assert
+        String formattedResult = resultFormat.format(result);
+        assertEquals("21/03/2023 00:00", formattedResult);
 
-        } catch (Exception ex) {
-            assert(false);
-        }
     }
 
+    @Tag("invalid")
     @Test
-    void getDateMergedWithTime_InvalidDate_Exception_BVA_1(){
+    void getDateMergedWithTime_InvalidDate_Exception_BVA_1() {
         Exception exception = assertThrows(IllegalArgumentException.class, () -> {
             dateService.getDateMergedWithTime("20:30", null);
         });
@@ -152,10 +146,12 @@ class DateServiceTest {
         assertEquals(dateErrMsg, errorMessage);
     }
 
+    @Tag("invalid")
     @Test
-    void getDateMergedWithTime_InvalidTime_Exception_BVA_2(){
+    @Disabled
+    void getDateMergedWithTime_InvalidTime_Exception_BVA_2() {
         Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-           dateService.getDateMergedWithTime("23:67", new Date());
+            dateService.getDateMergedWithTime("23:67", new Date());
         });
 
         String errorMessage = exception.getMessage();
